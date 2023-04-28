ec2ce2afea467091ea53218f4c5efb8ddf48fce8
ZakharEliseev <eliseev.zv@gmail.com>
2023-04-28 17:27:24 +0500

    add changes

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from app import app, db
-# from app.models import User
+from app.models import User
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db}
+    return {'db': db, 'user': User}
