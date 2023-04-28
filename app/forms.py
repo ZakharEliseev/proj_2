e64e3e9a448e99cea50debab5ab0368a4e1f1723
ZakharEliseev <eliseev.zv@gmail.com>
2023-04-28 16:47:49 +0500

    add changes

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
+    username = StringField('Логин', validators=[DataRequired(message='Введите свое имя пользователя')])
+    password = PasswordField('Пароль', validators=[DataRequired(message='Введите свой пароль')])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
