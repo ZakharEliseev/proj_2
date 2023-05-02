212d8ea4ff2f210444e7bb797fe373455b755bc4
ZakharEliseev <eliseev.zv@gmail.com>
2023-05-02 17:03:21 +0500

    db

---
+++ app/create_db.py
@@ -0,0 +1,13 @@
+from app import db, app
+from app.models import User
+
+
+def main():
+    with app.app_context():
+        u = User(username='test', email='test@example.com', password_hash='test')
+        db.session.add(u)
+        db.session.commit()
+
+
+if __name__ == '__main__':
+    main()
